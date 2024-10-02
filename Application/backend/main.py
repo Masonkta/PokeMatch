1b82145c919a153e42cd8f5b0f3992b6894eca0e
sjohn65189 <70227912+sjohn65189@users.noreply.github.com>
2024-10-02 14:56:53 -0500

    added a pokemon data file for the pokemon

--- Application/backend/main.py
+++ Application/backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from py2neo import Graph
 from pydantic import BaseModel
 import os
+from pokemon_data import data
 
 app = FastAPI()
 
@@ -35,17 +36,9 @@ async def create_profile(profile: Pokemon):
     graph.run(query, pokemon=profile.pokemon, type=profile.type, bio=profile.bio)
     return {"message": f"Profile created for {profile.pokemon}!"}
 
-# Define the Pokemon data to preload
-initial_data = [
-    {"pokemon": "Ditto", "type": "Normal", "bio": "I am a pink blob"},
-    {"pokemon": "Grookey", "type": "Grass", "bio": "Monkey Time"},
-    {"pokemon": "Malamar", "type": "Dark/Psychic", "bio": "Mysterious, Destructive, Curious, Bizzare creature who is on a mission to reshape humanity"},
-    # Add more Pokémon as needed
-]
-
 # Function to preload data into Neo4j
 def preload_pokemon_data():
-    for pokemon in initial_data:
+    for pokemon in data:
         query = """
         MERGE (p:Pokemon {name: $pokemon, type: $type, bio: $bio}) 
         RETURN p
---
+++ Application/backend/pokemon_data.py
@@ -0,0 +1,7 @@
+# Define the Pokemon data to preload
+data = [
+    {"pokemon": "Ditto", "type": "Normal", "bio": "I am a pink blob"},
+    {"pokemon": "Grookey", "type": "Grass", "bio": "Monkey Time"},
+    {"pokemon": "Malamar", "type": "Dark/Psychic", "bio": "Mysterious, Destructive, Curious, Bizzare creature who is on a mission to reshape humanity"},
+    # Add more Pokémon as needed
+]
