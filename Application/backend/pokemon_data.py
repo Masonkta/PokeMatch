706033ff69f54658df67c9913c64668035581221
Caje Auchard <caje.auch@gmail.com>
2024-10-02 15:17:49 -0500

    Updated pokemon list
    Added 30 more pokemon to the list.

--- Application/backend/pokemon_data.py
+++ Application/backend/pokemon_data.py
@@ -1,7 +1,29 @@
 # Define the Pokemon data to preload
 data = [
-    {"pokemon": "Ditto", "type": "Normal", "bio": "I am a pink blob"},
-    {"pokemon": "Grookey", "type": "Grass", "bio": "Monkey Time"},
-    {"pokemon": "Malamar", "type": "Dark/Psychic", "bio": "Mysterious, Destructive, Curious, Bizzare creature who is on a mission to reshape humanity"},
+    {"pokemon": "Ditto", "image":"https://assets.pokemon.com/assets/cms2/img/pokedex/full//132.png","type": "Normal", "bio": "I am a pink blob"},
+    {"pokemon": "Grookey", "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLx94R3D2o1WqiOYYQw07Z_UDCBywD-AahpQ&s","type": "Grass", "bio": "Professional Taxidermist and knife collector. Hoping to find a trainer who will help him complete his collections."},
+    {"pokemon": "Malamar", "image":"https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/687.png","type": "Dark/Psychic", "bio": "Mysterious, Destructive, Curious, Bizzare creature who is on a mission to reshape humanity"},
     # Add more Pokémon as needed
+    {"pokemon": "Pikachu", "image":"https://pokemonletsgo.pokemon.com/assets/img/common/char-pikachu.png","type": "Electric", "bio": "Gets pretty tired from using their electricity to charge their phone. Blames it on their loud neighbors."},
+    {"pokemon": "Charizard", "image":"https://assets.pokemon.com/assets/cms2/img/pokedex/full//006.png","type": "Fire/Flying", "bio": "Might looks ferocious, but has a warm heart and loves to take casual flights at night."},
+    {"pokemon": "Bulbasaur", "image":"https://assets.pokemon.com/assets/cms2/img/pokedex/full//001.png","type": "Grass/Poison", "bio": ""},
+    {"pokemon": "Squirtle", "image":"https://miro.medium.com/v2/resize:fit:750/1*W_ITBT0_dDNGyy_yPRQO3g.jpeg","type": "Water", "bio": ""},
+    {"pokemon": "Eevee", "image":"https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/133.png","type": "Normal", "bio": ""},
+    {"pokemon": "Frosslass", "image":"https://assets.pokemon.com/assets/cms2/img/pokedex/full//478.png","type": "Ice/Ghost", "bio": ""}
+    # {"pokemon": "Mewtwo", "type": "Psychic", "bio": ""},
+    # {"pokemon": "Lucario", "type": "Fighting/Steel", "bio": ""},
+    # {"pokemon": "Snorlax", "type": "Normal", "bio": ""},
+    # {"pokemon": "Gengar", "type": "Ghost/Poison", "bio": ""},
+    # {"pokemon": "Jigglypuff", "type": "Normal/Fairy", "bio": ""},
+    # {"pokemon": "Machamp", "type": "Fighting", "bio": ""},
+    # {"pokemon": "Arcanine", "type": "", "bio": ""},
+    # {"pokemon": "Dragonite", "type": "", "bio": ""},
+    # {"pokemon": "Blastoise", "type": "", "bio": ""},
+    # {"pokemon": "Venusaur", "type": "", "bio": ""},
+    # {"pokemon": "Lugia", "type": "", "bio": ""},
+    # {"pokemon": "Ho-Oh", "type": "", "bio": ""},
+    # {"pokemon": "Umbreon", "type": "", "bio": ""},
+    # {"pokemon": "Espeon", "type": "", "bio": ""},
+
+
 ]
