85b9cf531d32ecf9fb75d64855b3067ec0d18017
Caje Auchard <caje.auch@gmail.com>
2024-10-27 21:09:53 -0500

    Update matching
    Algorithm expects types as a list, but they are not currently formatted that way, so matching uses a debug value for rating for the moment

--- Application/backend/main.py
+++ Application/backend/main.py
@@ -191,30 +191,30 @@ async def matching(id: int):
     """
     result = graph.run(query,id=id).data()
     
-    userTypes = result[0]['u']['type']
-    userNatures = result[0]['u']['natures']
-    pokeTypes = result[0]['p']['type']
+    #userTypes = result[0]['u']['selectedTypes']
+    userNatures = result[0]['u']['selectedNatures']
+    #pokeTypes = result[0]['p']['type']
     pokeNatures = result[0]['p']['natures']
     rating = 0
-    typePoints = 0
+    #typePoints = 0
     natPoints = 0
     
     
-    if len(userTypes) >= len(pokeTypes):
-        denomTypes = len(userTypes)
-    else:
-        denomTypes = len(pokeTypes)
+    # if len(userTypes) >= len(pokeTypes):
+    #     denomTypes = len(userTypes)
+    # else:
+    #     denomTypes = len(pokeTypes)
     
     if len(userNatures) >= len(pokeNatures):
         denomNatures = len(userNatures)
     else:
         denomNatures = len(pokeNatures)
     
-    for i in range(len(userTypes)):
-        cur = userTypes[i]
-        if cur in pokeTypes:
-            typePoints+=1
-            continue
+    # for i in range(len(userTypes)):
+    #     cur = userTypes[0][i]
+    #     if cur in pokeTypes:
+    #         typePoints+=1
+    #         continue
         
     for i in range(len(userNatures)):
         cur = userNatures[i]
@@ -222,8 +222,9 @@ async def matching(id: int):
             natPoints+=1
             continue
     
-    rating = (typePoints/denomTypes) * .6 + (natPoints/denomNatures) * .4
-    # rating = 1 # Debug value
+    #rating = (typePoints/denomTypes) * .6 + (natPoints/denomNatures) * .4
+    #rating = natPoints/denomNatures
+    rating = 1 # Debug value
     
     if rating >= .7:
         return {"matchSuccess": "Pokemon match success"}
